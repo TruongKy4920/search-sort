b7382fae3fa1afd413deb99d7ce242e412d7e790
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-07-31 15:46:41 +0700

    5.6.txt

--- format.cpp
+++ format.cpp
@@ -4,6 +4,8 @@
 #include <sstream>
 #include <iomanip>
 #include <math.h>
+#include <algorithm>
+
 #define MAX 100
 using namespace std;
 
@@ -11,22 +13,32 @@ std::ifstream input("input.txt");
 std::ofstream output("output.txt");
 
 int a[MAX];
-int n,c;
+int b[MAX];
+int n,m;
 
 int solution() {
-   for(int i=0;i<n-1;i++){
-    for(int j=i+1;j<=n-1;j++){
-        if(a[i]==a[j]) c++;
+    int i,j,count=0;
+    i=j=count;
+    sort(a,a+n);
+    sort(b,b+n);
+    while(i<n && j<m){
+        if(a[i]>b[j]){
+            j++;
+            count++;
+        }
+        i++;
     }
-   }
-   return c;
+    return count;
 }
 
 int main(){
     std::string line;
-    input>>n;
+    input>>n>>m;
       for(int i=0;i<n;i++){
         input>>a[i];
+      }
+       for(int i=0;i<m;i++){
+        input>>b[i];
       }
     
     
