fcf044dbd7b9f1d39da63ef42e4087ca2f3903ff
TruongKy4920 <109138035+TruongKy4920@users.noreply.github.com>
2025-07-31 22:55:17 +0700

    update 5.7

--- format.cpp
+++ format.cpp
@@ -12,34 +12,59 @@ using namespace std;
 std::ifstream input("input.txt");
 std::ofstream output("output.txt");
 
+int n, p;
 int a[MAX];
-int b[MAX];
-int n,m;
+int min_list[MAX]; // min_list[i] sẽ lưu giá trị nhỏ nhất trong dãy a[1], a[2],... a[i]
 
-int solution() {
-    int i,j,count=0;
-    i=j=count;
-    sort(a,a+n);
-    sort(b,b+n);
-    while(i<n && j<m){
-        if(a[i]>b[j]){
-            j++;
-            count++;
-        }
-        i++;
-    }
-    return count;
+int min(int u, int v) {
+    return u < v ? u : v;
 }
+//minlist= 4 3 3 2 2 2 
+//         1 2 3 4 5 6 
+int binary_search(int i, int j) { // Tìm
+    int start = i; //1
+    int end = j-1; //1
+    int x = j; //2 
+
+    while (start <= end) {
+        int mid = (start+end)/2; //1
+
+
+        if(a[j] - min_list[mid] < p) {  // 3-4 
+            start = mid + 1;
+        }
+        else {
+            x = mid; //x=1
+            end = mid - 1; //end =0
+        }
+    }
+    return x;
+}
+
+
+void solution() {
+    min_list[1] = a[1];
+    for(int i = 2; i <=n; i++)
+        min_list[i] = min(min_list[i-1], a[i]);
+
+    int result = 0;
+   
+    for(int j = 2; j <= n; j++) {
+        int i = binary_search(1, j);
+        if (j-i > result)
+            result = j-i;
+    }
+    std::cout << result;
+}
+
 
 int main(){
     std::string line;
-    input>>n>>m;
-      for(int i=0;i<n;i++){
+    input>>n>>p;
+      for(int i=1;i<=n;i++){
         input>>a[i];
       }
-       for(int i=0;i<m;i++){
-        input>>b[i];
-      }
+     
     
     
     output<<solution();
